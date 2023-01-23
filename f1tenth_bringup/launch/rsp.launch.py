13e836c6886bdbc807fee0c4d3ee9eb14c529a45
chris17432 <chrisgraham908@gmail.com>
2023-01-23 15:11:30 +1300

    rsp works and now.

--- f1tenth_bringup/launch/rsp.launch.py
+++ f1tenth_bringup/launch/rsp.launch.py
@@ -3,7 +3,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.conditions import IfCondition
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 
@@ -11,67 +12,65 @@ import xacro
 
 
 def generate_launch_description():
-    # Check if we're told to use sim time
+    # ---------- RSP ----------
+
     use_sim_time = LaunchConfiguration('use_sim_time')
-    use_jsp_gui = LaunchConfiguration('use_rjp_gui')
-    use_rviz = LaunchConfiguration('use_rviz')
-    rviz_config = LaunchConfiguration('rviz_config')
+    use_sim_time_arg = DeclareLaunchArgument(
+        name='use_sim_time',
+        default_value='false',
+        description='Use sim time if true'
+    )
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('f1tenth_description'))
-    xacro_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro')
-    robot_description_config = xacro.process_file(xacro_file)
+    description_pkg_path = os.path.join(get_package_share_directory('f1tenth_description'))
+    xacro_file = os.path.join(description_pkg_path, 'urdf', 'robot.urdf.xacro')
+    robot_description = xacro.process_file(xacro_file)
 
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    rsp_params = {'robot_description': robot_description.toxml(), 'use_sim_time': use_sim_time}
     robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[rsp_params]
+    )
+
+    # ---------- JSP ----------
+    use_jsp_gui = LaunchConfiguration('use_jsp_gui')
+    use_jsp_gui_arg = DeclareLaunchArgument(
+        name='use_jsp_gui',
+        default_value='false',
+        description='Loads joint state publisher gui if true'
     )
 
     joint_state_publisher_gui = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
-        output='screen'
+        output='screen',
+        condition=IfCondition(use_jsp_gui),
     )
+
+    # ---------- RVIZ ---------
+    use_rviz = LaunchConfiguration('use_rviz')
+    use_rviz_arg = DeclareLaunchArgument(
+        name='use_rviz',
+        default_value='false',
+        description='Loads Rviz2 if True')
 
     rviz = Node(
         package='rviz2',
         executable='rviz2',
         output='screen',
-        parameters=['-d' + os.path.join(get_package_share_directory(pkg_path), 'rviz', rviz_config)]
+        parameters=[f'-d {os.path.join(description_pkg_path, "rviz", "config.rviz" )}'],
+        condition=IfCondition(use_rviz)
     )
 
-    launch_list = [
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
+    return LaunchDescription([
+        # Launch configs
+        use_sim_time_arg,
+        use_jsp_gui_arg,
+        use_rviz_arg,
 
-        DeclareLaunchArgument(
-            'use_rjp_gui',
-            default_value='False',
-            description='Loads joint state publisher gui if true'),
-
-        DeclareLaunchArgument(
-            'use_rviz',
-            default_value='False',
-            description='Loads Rviz if True'),
-
-        DeclareLaunchArgument(
-            'rviz_config',
-            default_value='default.rviz',
-            description='rviz2 config file'),
-
+        # Nodes
         robot_state_publisher,
-    ]
-
-    if use_jsp_gui:
-        launch_list.append(joint_state_publisher_gui)
-    if use_rviz:
-        launch_list.append(rviz)
-
-    # Launch!
-    return LaunchDescription(launch_list)
+        joint_state_publisher_gui,
+        rviz,
+    ])
