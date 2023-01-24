e95ab0cafc19d2e73677660a3b29256be8771029
chris17432 <chrisgraham908@gmail.com>
2023-01-25 12:00:06 +1300

    I HATE FLAGS WITH A PASSION!

--- f1tenth_bringup/launch/sim.launch.py
+++ f1tenth_bringup/launch/sim.launch.py
@@ -6,31 +6,29 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-from launch_ros.actions import Node
-
 
 def generate_launch_description():
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
     pkg_f1tenth_bringup = get_package_share_directory('f1tenth_bringup')
 
-    gz_sim = IncludeLaunchDescription(
+    rsp = IncludeLaunchDescription(
         launch_description_source=PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py'))
+            os.path.join(pkg_f1tenth_bringup, 'rsp.launch.py')),
+        launch_arguments={
+            'use_sim_time': 'true',
+            'use_jsp_gui': 'true',
+        }.items(),
     )
 
-    gz_bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        parameters=[{
-            # TODO: Submit issue to https://github.com/gazebosim/ros_gz/issues cuz this shit aint working
-            # 'config_file': os.path.join(pkg_f1tenth_bringup, 'config', 'ros_gz_example_bridge.yaml')
-
-            # TODO: Bridge cmd and other topics
-        }],
-        output='screen'
+    gz_sim = IncludeLaunchDescription(
+        launch_description_source=PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')),
+        launch_arguments={
+            'gz_args': 'empty.sdf',
+        }.items()
     )
 
     return LaunchDescription([
+        rsp,
         gz_sim,
-        gz_bridge,
     ])
---
+++ f1tenth_bringup/launch/spawn.launch.py
@@ -0,0 +1,49 @@
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, OpaqueFunction
+
+
+def spawn_func(context, *args, **kwargs):
+    world = LaunchConfiguration('world').perform(context)
+    name = LaunchConfiguration('name').perform(context)
+    topic = LaunchConfiguration('topic').perform(context)
+
+    cmd_exec = 'ros2 run ros_gz_sim create'
+
+    return [ExecuteProcess(
+        cmd=[cmd_exec,
+             '-world', f"'{world}'",
+             '-name', f"'{name}'",
+             '-topic', f"'{topic}'",
+             ],
+        output='screen',
+        shell=True,
+    )]
+
+
+def generate_launch_description():
+    world_arg = DeclareLaunchArgument(
+        name='world',
+        default_value='',
+        description='leave blank unless loading into specified world'
+    )
+
+    name_arg = DeclareLaunchArgument(
+        name='name',
+        default_value='',
+        description='name of f1tenth car spawned'
+    )
+
+    topic_arg = DeclareLaunchArgument(
+        name='topic',
+        default_value='/robot_description',
+        description='specifies xml topic to spawn from'
+    )
+
+    return LaunchDescription([
+        world_arg,
+        name_arg,
+        topic_arg,
+
+        OpaqueFunction(function=spawn_func)
+    ])
