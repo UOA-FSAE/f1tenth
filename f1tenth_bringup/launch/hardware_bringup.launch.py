3fabbc1a8014a9c9b7825c7cbb062cd29e90e0a0
Jack17432 <ChrisGraham908@gmail.com>
2023-04-04 13:54:29 +1200

    Created hardware launch

---
+++ f1tenth_bringup/launch/hardware_bringup.launch.py
@@ -0,0 +1,49 @@
+import os
+
+from launch_ros.substitutions import FindPackageShare
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution, TextSubstitution
+
+
+def generate_launch_description():
+
+    # launch vesc
+    vesc_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('vesc_driver'),
+                'launch',
+                'vesc_driver_node.launch.py'
+            ])
+        ])
+    )
+
+    ackermann_to_vesc_launch = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('vesc_ackermann'),
+                'launch',
+                'ackermann_to_vesc_node.launch.xml'
+            ])
+        ])
+    )
+
+    vesc_to_odom_launch = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('vesc_ackermann'),
+                'launch',
+                'vesc_to_odom_node.launch.xml'
+            ])
+        ])
+    )
+
+    return LaunchDescription([
+        ackermann_to_vesc_launch,
+        vesc_to_odom_launch,
+        vesc_launch,
+    ])
