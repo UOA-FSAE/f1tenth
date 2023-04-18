f77569ddd186d62bc197b0e2a5d1d51fb354e4f0
retinfai <aferetipama@gmail.com>
2023-04-18 16:53:25 +1200

    feat: add parameter bridge to launch

--- simulation_drivers/simulation/launch/test.launch.py
+++ simulation_drivers/simulation/launch/test.launch.py
@@ -1,10 +1,33 @@
 from launch import LaunchDescription 
 from launch_ros.actions import Node 
 import launch
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.actions import ExecuteProcess, DeclareLaunchArgument, OpaqueFunction, IncludeLaunchDescription, \
+    SetEnvironmentVariable
+
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
+    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
+    return LaunchDescription([
+        Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            f'/world/empty/control@ros_gz_interfaces/srv/ControlWorld',
+            f'/world/empty/create@ros_gz_interfaces/srv/SpawnEntity',
+            f'/world/empty/remove@ros_gz_interfaces/srv/DeleteEntity',
+            f'/world/empty/set_pose@ros_gz_interfaces/srv/SetEntityPose',
+        ]
+    ),
         Node(
             package='simulation',
             executable='gz_services',
