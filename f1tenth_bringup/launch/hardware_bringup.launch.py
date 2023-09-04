c4c8895f3ac6f375cd71e59147e7169ac8d2933c
Chris17432 <ChrisGraham908@gmail.com>
2023-09-04 17:17:57 +1200

    completed the car now it go brrrrrr

--- f1tenth_bringup/launch/hardware_bringup.launch.py
+++ f1tenth_bringup/launch/hardware_bringup.launch.py
@@ -10,46 +10,98 @@ from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution, TextSubstitution, LaunchConfiguration
 
-
 def generate_launch_description():
+	joy_config = os.path.join(
+		get_package_share_directory('f1tenth_bringup'),
+		'config',
+		'joy.yaml'
+	)
+	vesc_config = os.path.join(
+		get_package_share_directory('f1tenth_bringup'),
+		'config',
+		'vesc.yaml'
+	)
+	sensors_config = os.path.join(
+		get_package_share_directory('f1tenth_bringup'),
+		'config',
+		'sensors.yaml'
+	)
+	mux_config = os.path.join(
+		get_package_share_directory('f1tenth_bringup'),
+		'config',
+		'mux.yaml'
+	)
 
-    '''
-    inputs:
-        name: name of f1tenth car
-        vesc_config: overides defult config (this contains the topic names as well)
-    '''
-    pkg_vesc_ackermann = get_package_share_directory('vesc_ackermann')
-    # launch vesc
-    vesc_config = os.path.join(
-        get_package_share_directory('vesc_driver'),
-        'params',
-        'vesc_config.yaml'
-    )
+	joy_la = DeclareLaunchArgument(
+		'joy_config',
+		default_value=joy_config,
+		description='Descriptions for joy and joy_teleop configs')
+	vesc_la = DeclareLaunchArgument(
+		'vesc_config',
+		default_value=vesc_config,
+		description='Descriptions for vesc configs')
+	sensors_la = DeclareLaunchArgument(
+		'sensors_config',
+		default_value=sensors_config,
+		description='Descriptions for sensor configs')
+	mux_la = DeclareLaunchArgument(
+		'mux_config',
+		default_value=mux_config,
+		description='Descriptions for ackermann mux configs')
 
-    return LaunchDescription([
-        DeclareLaunchArgument(
-            name="config",
-            default_value=vesc_config,
-            description="VESC yaml configuration file.",
-        ),
-		Node(
-		    package='vesc_driver',
-		    executable='vesc_driver_node',
-		    name='vesc_driver_node',
-		    namespace='f1tenth',
-		    parameters=[LaunchConfiguration("config")]
-		),
-		PushRosNamespace(
-        	namespace='f1tenth',
-		    launch_descrption=[
-				IncludeLaunchDescription(
-					launch_description_source=XMLLaunchDescriptionSource(
-						os.path.join(pkg_vesc_ackermann, 'launch', 'ackermann_to_vesc_node.launch.xml'),
-				)),
-				IncludeLaunchDescription(
-					launch_description_source=XMLLaunchDescriptionSource(
-						os.path.join(pkg_vesc_ackermann, 'launch', 'vesc_to_odom_node.launch.xml'),	
-				))
-		    ],
-        )
-    ])
+	ld = LaunchDescription([joy_la, vesc_la, sensors_la, mux_la])
+
+	joy_node = Node(
+		package='joy',
+		executable='joy_node',
+		name='joy',
+		parameters=[LaunchConfiguration('joy_config')]
+	)
+	joy_teleop_node = Node(
+		package='joy_teleop',
+		executable='joy_teleop',
+		name='joy_teleop',
+		parameters=[LaunchConfiguration('joy_config')]
+	)
+	ackermann_to_vesc_node = Node(
+		package='vesc_ackermann',
+		executable='ackermann_to_vesc_node',
+		name='ackermann_to_vesc_node',
+		parameters=[LaunchConfiguration('vesc_config')]
+	)
+	vesc_to_odom_node = Node(
+		package='vesc_ackermann',
+		executable='vesc_to_odom_node',
+		name='vesc_to_odom_node',
+		parameters=[LaunchConfiguration('vesc_config')]
+	)
+	vesc_driver_node = Node(
+		package='vesc_driver',
+		executable='vesc_driver_node',
+		name='vesc_driver_node',
+		parameters=[LaunchConfiguration('vesc_config')]
+	)
+	urg_node = Node(
+		package='urg_node',
+		executable='urg_node_driver',
+		name='urg_node',
+		parameters=[LaunchConfiguration('sensors_config')]
+	)
+	ackermann_mux_node = Node(
+		package='ackermann_mux',
+		executable='ackermann_mux',
+		name='ackermann_mux',
+		parameters=[LaunchConfiguration('mux_config')],
+		remappings=[('ackermann_cmd_out', 'ackermann_cmd')]
+	)
+
+	# finalize
+	# ld.add_action(joy_node)
+	ld.add_action(joy_teleop_node)
+	ld.add_action(ackermann_to_vesc_node)
+	ld.add_action(vesc_to_odom_node)
+	ld.add_action(vesc_driver_node)
+	ld.add_action(ackermann_mux_node)
+	ld.add_action(urg_node)
+
+	return ld
